6fda066e7a449610010bf6a7b616015dd1b6d919
serjramone <sergey.greznov@gmail.com>
2018-10-19 17:17:36 +0200

    враг + путь движения

---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveEnemy : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject[] waypoints;
+    public float speed = 1.0f;
+
+    private int currentWaypoint = 0;
+    private float lastWaypointSwitchTime;
+
+    private void Start()
+    {
+        lastWaypointSwitchTime = Time.time;
+    }
+
+    private void Update()
+    {
+        Vector3 startPosition = waypoints[currentWaypoint].transform.position;
+        Vector3 endPosition = waypoints[currentWaypoint + 1].transform.position;
+
+        float pathLength = Vector3.Distance(startPosition, endPosition);
+        float totalTimeForPath = pathLength / speed;
+        float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+        gameObject.transform.position = Vector2.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+
+        if (gameObject.transform.position.Equals(endPosition))
+        {
+            if (currentWaypoint < waypoints.Length - 2)
+            {
+                currentWaypoint++;
+                lastWaypointSwitchTime = Time.time;
+                // TODO: поворачиваться в направлении движения
+            }
+            else
+            {
+                Destroy(gameObject);
+
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+                // TODO: вычитать здоровье
+            }
+        }
+    }
+}
