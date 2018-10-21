84872353df2ac075697622c4c2b0b44d1254b3aa
serjramone <sergey.greznov@gmail.com>
2018-10-21 16:35:08 +0200

    Отображение номера волны

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -6,8 +6,12 @@ using UnityEngine.UI;
 public class GameManagerBehavior : MonoBehaviour
 {
     public Text goldLabel;
+    public Text waveLabel;
+    public GameObject[] nextWaveLabels;
+    public bool gameOver = false;
 
     private int gold;
+    private int wave;
 
     public int Gold
     {
@@ -22,8 +26,29 @@ public class GameManagerBehavior : MonoBehaviour
         }
     }
 
+    public int Wave
+    {
+        get
+        {
+            return wave;
+        }
+        set
+        {
+            wave = value;
+            if (!gameOver)
+            {
+                for (int i = 0; i < nextWaveLabels.Length; i++)
+                {
+                    nextWaveLabels[i].GetComponent<Animator>().SetTrigger("nextWave");
+                }
+            }
+            waveLabel.text = "WAVE: " + (wave + 1);
+        }
+    }
+
     private void Start()
     {
         Gold = 1000;
+        Wave = 0;
     }
 }
