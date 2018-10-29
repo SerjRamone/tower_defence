93f9275c3615ada8d41d37af2315bd906f94f7fd
serjramone <sergey.greznov@gmail.com>
2018-10-29 22:55:47 +0100

    стрельба по врагам

--- Assets/Scripts/ShootEnemies.cs
+++ Assets/Scripts/ShootEnemies.cs
@@ -6,9 +6,52 @@ public class ShootEnemies : MonoBehaviour
 {
     public List<GameObject> enemiesInRange;
 
+    private float lastShotTime;
+    private MonsterData monsterData;
+
     private void Start()
     {
         enemiesInRange = new List<GameObject>();
+        lastShotTime = Time.time;
+        monsterData = gameObject.GetComponentInChildren<MonsterData>();
+    }
+
+    private void Update()
+    {
+        GameObject target = null;
+
+        //Определяем цель монстра.Начинаем с максимально возможного расстояния 
+        //в minimalEnemyDistance.Обходим в цикле всех врагов в радиусе действия 
+        //и делаем врага новой целью, если его расстояние до печеньки меньше 
+        //текущего наименьшего.
+        float minimalEnemyDistance = float.MaxValue;
+        foreach (GameObject enemy in enemiesInRange)
+        {
+            float distanceToGoal = enemy.GetComponent<MoveEnemy>().DistanceToGoal();
+            if (distanceToGoal < minimalEnemyDistance)
+            {
+                target = enemy;
+                minimalEnemyDistance = distanceToGoal;
+            }
+        }
+
+        //Вызываем Shoot, если прошедшее время больше частоты стрельбы монстра 
+        //и задаём lastShotTime значение текущего времени.
+        if (target != null)
+        {
+            if (Time.time - lastShotTime > monsterData.CurrentLevel.fireRate)
+            {
+                Shoot(target.GetComponent<Collider2D>());
+                lastShotTime = Time.time;
+            }
+
+            //Вычисляем угол поворота между монстром и его целью. Выполняем 
+            //поворот монстра на этот угол. Теперь он всегда будет смотреть на цель.
+            Vector3 direction = gameObject.transform.position - target.transform.position;
+            gameObject.transform.rotation = Quaternion.AngleAxis(
+                Mathf.Atan2(direction.y, direction.x) * 180 / Mathf.PI,
+                new Vector3(0, 0, 1));
+        }
     }
 
     void OnEnemyDestroy(GameObject enemy)
@@ -36,5 +79,28 @@ public class ShootEnemies : MonoBehaviour
                 other.gameObject.GetComponent<EnemyDestructionDelegate>();
             del.enemyDelegate -= OnEnemyDestroy;
         }
+    }
+
+    void Shoot(Collider2D target)
+    {
+        GameObject bulletPrefab = monsterData.CurrentLevel.bullet;
+
+        Vector3 startPosition = gameObject.transform.position;
+        Vector3 targetPosition = target.transform.position;
+        startPosition.z = bulletPrefab.transform.position.z;
+        targetPosition.z = bulletPrefab.transform.position.z;
+
+        GameObject newBullet = (GameObject)Instantiate(bulletPrefab);
+        newBullet.transform.position = startPosition;
+        BulletBehavior bulletComp = newBullet.GetComponent<BulletBehavior>();
+        bulletComp.target = target.gameObject;
+        bulletComp.startPosition = startPosition;
+        bulletComp.targetPosition = targetPosition;
+
+        Animator animator =
+            monsterData.CurrentLevel.visualization.GetComponent<Animator>();
+        animator.SetTrigger("fireShot");
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.PlayOneShot(audioSource.clip);
     }
 }
